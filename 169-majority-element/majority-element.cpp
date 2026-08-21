6ead5ac7b99f01f0ec3620e12c6ce8ca07d7628c
anshuman2502 <153819474+anshuman2502@users.noreply.github.com>
2026-08-21 19:01:44 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (75.93%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        for(int i:nums){
-            mpp[i]++;
-        }
-        int maxi=INT_MIN;
-        int res;
-        for(auto&[idx,i]:mpp){
-            if(maxi<i){
-                maxi=i;
-                res=idx;
+        int maxi=1;
+        int num=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(maxi==0)num=nums[i];
+            if(nums[i]==num){
+                maxi++;
+            }else{
+                maxi--;
             }
+
         }
-        return res;
+        return num;
     }
 };
