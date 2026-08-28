6dd387bd5b5d494d8f6ad01d8fc9bcd82f88598f
anshuman2502 <153819474+anshuman2502@users.noreply.github.com>
2026-08-28 22:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (51.41%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n=s.size();
-        string temp="";
         string ans="";
     reverse(s.begin(),s.end());
         for(int i=0;i<n;i++){
-            if(s[i]!=' ')temp+=s[i];
-            else if(s[i]==' ' && !temp.empty()){
-                reverse(temp.begin(),temp.end());
-                ans+=temp;
-                temp="";
-                ans+=' ';
-            }
-            if(i==n-1){
-              if(!temp.empty()){
-                 reverse(temp.begin(),temp.end());
-                ans+=temp; 
-                ans+=' ';
-              }
-            }
-            }  
+            string temp="";
+          while(i<n && s[i]!=' '){
+            temp+=s[i];
+            i++;
+          }
+          if(!temp.empty()){
+            reverse(temp.begin(),temp.end());
+            ans+=temp;
+            ans+=' ';            
+          }
+            } 
            return ans.substr(0,ans.size()-1);  
     }
 };
