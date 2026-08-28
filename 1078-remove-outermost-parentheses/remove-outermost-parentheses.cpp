e4fd331a8d17ae12fcaa4417a75d410acb22bb9a
anshuman2502 <153819474+anshuman2502@users.noreply.github.com>
2026-08-28 20:32:50 +0530

    Time: 3 ms (17.11%) | Memory: 9 MB (23.21%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -3,19 +3,15 @@ public:
     string removeOuterParentheses(string s) {
         int cnt=0;
         string res = "";
-        for(char a:s){
-            if(a=='('){
-                if(cnt>=1){
-                    res+='(';
-                    cnt++;
-                }else cnt++;
-            }else {
-                if(cnt>1){
-                    res+=')';
-                    cnt--;
-                }else cnt--;
-            }
+        for(char c:s){
+            if(c == '('){
+                cnt++;
+            if(cnt>1)res +='(';
+        }else if(c==')'){
+            cnt--;
+            if(cnt>0)res+=')';
         }
-        return res;        
+        }        
+        return res;
     }
 };
