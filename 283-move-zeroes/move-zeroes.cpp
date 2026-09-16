d9bcf3567b8d53bf01f03536da53183f926c7719
anshuman2502 <153819474+anshuman2502@users.noreply.github.com>
2026-09-16 23:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.51%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>ans(nums.size(),0);
         int i=0;
-        for(int j=0;j<nums.size();j++){
-            if(nums[j]!=0){
-                ans[i]=nums[j];
-                i++;
-            }
-        }        
-        nums = ans;
+       while(i<nums.size()){
+        if(nums[i]==0)break;
+        i++;
+       }
+       for(int j=i+1;j<nums.size();j++){
+        if(nums[j]!=0){
+            swap(nums[j],nums[i]);
+            i++;
+        }
+       }
     }
 };
