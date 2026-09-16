8764fc1e8adbe87a63fc5ee91e451aa71bb06c70
anshuman2502 <153819474+anshuman2502@users.noreply.github.com>
2026-09-16 20:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 263.5 MB (19.11%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,6 +4,7 @@ public:
         k=k%nums.size();
         reverse(nums.begin(),nums.end());
         reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());        
+        reverse(nums.begin()+k,nums.end());
+        
     }
 };
